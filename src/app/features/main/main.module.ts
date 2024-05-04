e03648fdf2ad438717bf351c5c33a4ea3635e928
rafa <rafakmp18@gmail.com>
2024-05-04 19:34:15 +0200

    feat: add banner points

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainRoutingModule } from './main-routing.module';
-import { MenuComponent } from './pages/menu/menu.component';
+import { MenuComponent } from '../menu/menu.component';
 import { MainComponent } from './main.component';
+import { BannerPointsComponent } from './components/banner-points/banner-points.component';
 
 @NgModule({
-  declarations: [MenuComponent, MainComponent, NavbarComponent],
+  declarations: [MenuComponent, MainComponent, NavbarComponent, BannerPointsComponent],
   imports: [MainRoutingModule, CommonModule],
 })
 export class MainModule {}
