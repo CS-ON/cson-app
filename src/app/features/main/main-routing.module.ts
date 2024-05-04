91ce91fd29dd8f1b0b935a9c06a4f7f863c837f3
rafa <rafakmp18@gmail.com>
2024-05-04 19:38:08 +0200

    feat: edu guapo

--- src/app/features/main/main-routing.module.ts
+++ src/app/features/main/main-routing.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main.component';
-import { MenuComponent } from '../menu/menu.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
-    children: [
-      {
-        path: 'menu',
-        component: MenuComponent
-      }
-    ]
-  }
+    children: [],
+  },
 ];
 
 @NgModule({
