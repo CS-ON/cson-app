bdbc7c681cdaf39c82c94cfca8d9b432e2720c9f
Pablo Carballeda <pcarballeda@gmail.com>
2024-05-07 21:37:51 +0200

    Remove unused scss. Bump Tailwind version.

--- src/app/features/business/business.component.ts
+++ src/app/features/business/business.component.ts
@@ -1,10 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-business',
   templateUrl: './business.component.html',
-  styleUrls: ['./business.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BusinessComponent {}
