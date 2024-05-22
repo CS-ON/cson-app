2291c3141d7ce5c26420088791619d0e3faec828
Alex G <alexgas3@gmail.com>
2024-05-22 20:28:36 +0200

    add routing to obtain points

--- src/app/features/main/pages/score-points/score-points.component.ts
+++ src/app/features/main/pages/score-points/score-points.component.ts
@@ -1,16 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-score-points',
   templateUrl: './score-points.component.html',
-  styleUrls: ['./score-points.component.scss']
+  styleUrls: ['./score-points.component.scss'],
 })
 export class ScorePointsComponent {
   qrResultString: any;
+  constructor(private readonly router: Router) {}
 
   handleQrCodeResult(event: any) {
     console.log(typeof event, event);
     this.qrResultString = event;
+    this.router.navigate(['/main/obtain-points']);
   }
 }
